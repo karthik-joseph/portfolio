bafbf272583da8e4d88803e97851075362e85b89
karthik P <karthikkarthikp256@gmail.com>
2026-05-22 16:30:11 +0530

    Removed the basepath for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
 };
 
 export default nextConfig;
